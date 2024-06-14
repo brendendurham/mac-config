70bc13fc7d6162c1986c995799eb011fafb21aa8
Brenden Durham <paybdd@gmail.com>
2024-06-14 07:04:10 -0500

    Add global gitignore and setup script

--- git/setup.sh
+++ git/setup.sh
@@ -115,3 +115,5 @@ git config --global core.fsmonitor    true
 # https://github.com/libgit2/libgit2/issues/6531
 # To make powerlevel10k which uses libgit2 work properly
 git config --global index.skipHash false
+
+git config --global core.excludesfile '~/.gitignore_global'
